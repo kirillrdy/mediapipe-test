1c1374a9591adef7f791247d2870269077ea5e8a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-10-19 18:54:53 +1100

    clean up

--- flake.nix
+++ flake.nix
@@ -2,17 +2,13 @@
   description = "A very basic flake";
   inputs.nixpkgs.url = "github:benxiao/nixpkgs/mediapipe";
   outputs = { self, nixpkgs }: {
-
     packages.x86_64-linux.default =
       let
         pkgs = import nixpkgs { system = "x86_64-linux"; };
         python = (pkgs.python3.withPackages (p: [ p.mediapipe ]));
       in
-      pkgs.writeShellScriptBin "test" (
-        ''
-          ${python}/bin/python3 ${./example.py}
-        ''
-      );
-
+      pkgs.writeShellScriptBin "test" ''
+        ${python}/bin/python3 ${./example.py}
+      '';
   };
 }
